54761db55db673e4c62de800d08e8d1bf6b56963
Sharpicek <davidthomas13@proton.me>
2024-09-16 12:22:24 +0200

    IDK

--- script.js
+++ script.js
@@ -12,5 +12,26 @@ function addBookToLibrary (title, author, pages, read) {
      myLibrary.push(new Book(title, author, pages, read));
 };
 
-addBookToLibrary('Dune', 'Frank Herbert', '638', 'yes');
-console.log(myLibrary);
+addBookToLibrary('Duna', 'Frank Herbert', '638', true);
+addBookToLibrary('Harry Potter a Kámen Mudrců', 'J. K. Rowling', '246', true);
+
+function showLibrary() {
+    const table = document.getElementById("library");
+
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        const newRow = table.insertRow(i + 1);
+        let cellCount = 0;
+
+        for (let key in myLibrary[i]) {
+
+            const newCell = newRow.insertCell(cellCount);
+            const newText = document.createTextNode(myLibrary[i][key]);
+            newCell.appendChild(newText);
+            ++cellCount;
+
+        }
+    }
+}
+
+showLibrary();
