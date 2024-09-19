370d6dc61e775dfa9de225b530ab02b11532f38c
Sharpicek <davidthomas13@proton.me>
2024-09-19 16:49:38 +0200

    Finish library

--- script.js
+++ script.js
@@ -12,18 +12,34 @@ Book.prototype.toggleRead = function() {
 };
 
 
-function addBookToLibrary (title, author, pages, read) {
-     myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary () {
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    const pagesInput = document.getElementById("pages");
+    const readCheckbox = document.getElementById("check-read");
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readCheckbox.value;
+    myLibrary.push(new Book(title, author, pages, read));
+    dialog.close();
+    showLibrary();
+    clearInputFields();
 };
 
-addBookToLibrary("Dune", "Frank Herbert", "638", true);
-addBookToLibrary("Harry Potter and the Half-Blood Prince", "J. K. Rowling", "668", false);
+function clearInputFields () {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+}
 
 function showLibrary() {
     const table = document.getElementById("library");
 
-    for (let i = 0; i < myLibrary.length; i++) {
+    // Kontrola, kolik řádků už je v tabulce (bez hlavičky)
+    const currentRows = table.rows.length - 1;
 
+    for (let i = currentRows; i < myLibrary.length; i++) {
         const newRow = table.insertRow(i + 1);
         newRow.setAttribute("id",`row${i}`);
         let cellCount = 0;
@@ -83,5 +99,24 @@ function removeBook(row) {
     const deleteRow = document.getElementById(`row${row}`);
     deleteRow.remove();
 }
+
+// Dialog
+const dialog = document.getElementById("open-dialog");
+const showButton = document.getElementById("add-book-btn");
+const closeButton = document.getElementById("close-dialog");
+const addButton = document.getElementById("add-book-to-library");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+})
 
 showLibrary();
